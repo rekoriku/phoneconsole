2a28875f70278f591982e95f60da9f217f9086ee
rekoriku <rerikuju@gmail.com>
2019-06-02 16:14:21 +0300

    non finished database class

---
+++ server/Database.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace server
+{
+    class Database
+    {
+        string serverIp = "<Server IP or Hostname>";
+        string username = "<DB username>";
+        string password = "<DB password>";
+        string databaseName = "<DB name>";
+        public Database()
+        {
+           
+        }
+        
+
+        string dbConnectionString = string.Format("server={0};uid={1};pwd={2};database={3};", serverIp, username, password, databaseName);
+        string query = "SELECT * FROM YourTable";
+
+        MySqlConnection conn = new MySql.Data.MySqlClient.MySqlConnection(dbConnectionString);
+        conn.Open();
+ 
+MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(query, conn);
+        MySqlDataReader reader = cmd.ExecuteReader();
+
+ 
+while (reader.Read())
+{
+ someValue = reader["SomeColumnName"];
+
+        // Do something with someValue
+    }
+}
+
