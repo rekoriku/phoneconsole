d09f8e1231f38f5e6e16fe2d09298cd976ea6f04
Viisaspulla <30913249+Viisaspulla@users.noreply.github.com>
2019-06-02 16:00:43 +0300

    Added RequestType.

--- sharedfiles/Networking.cs
+++ sharedfiles/Networking.cs
@@ -4,32 +4,39 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
+enum RequestType
+{
+    add_rows = 0,
+    delete_rows = 1,
+    seach_all_rows = 2,
+    search_rows_by_name = 3,
+    seach_rows_by_number = 4,
+}
 
-    static class Networking
+static class Networking
+{
+    public static string ReadMessage(NetworkStream stream)
     {
-        public static string ReadMessage(NetworkStream stream)
+        if (stream != null && stream.CanRead)
         {
-            if (stream != null && stream.CanRead)
-            {
-                byte[] bytes = new byte[1024];
-                int bytesRead = stream.Read(bytes, 0, bytes.Length);
+            byte[] bytes = new byte[1024];
+            int bytesRead = stream.Read(bytes, 0, bytes.Length);
 
-                return Encoding.ASCII.GetString(bytes, 0, bytesRead);
-            }
-            else
-            {
-                return "";
-            }
+            return Encoding.ASCII.GetString(bytes, 0, bytesRead);
         }
-
-        public static void SendMessage(NetworkStream stream, string message)
+        else
         {
-            if (stream != null && stream.CanWrite)
-            {
-                byte[] clientMessageAsByteArray = Encoding.ASCII.GetBytes(message);
-                stream.Write(clientMessageAsByteArray, 0, clientMessageAsByteArray.Length);
-                Console.WriteLine("Client sent his message - should be received by server");
-            }
+            return "";
         }
     }
 
+    public static void SendMessage(NetworkStream stream, string message)
+    {
+        if (stream != null && stream.CanWrite)
+        {
+            byte[] clientMessageAsByteArray = Encoding.ASCII.GetBytes(message);
+            stream.Write(clientMessageAsByteArray, 0, clientMessageAsByteArray.Length);
+            Console.WriteLine("Client sent his message - should be received by server");
+        }
+    }
+}
