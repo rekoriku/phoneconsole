41e9eae0010b85b8916746f8d81eab797478c143
Viisaspulla <30913249+Viisaspulla@users.noreply.github.com>
2019-06-08 15:33:26 +0300

    Update Program.cs

--- server/Program.cs
+++ server/Program.cs
@@ -12,39 +12,43 @@ namespace server
         
         public static int Main(String[] args)
         {
-//             bool done = false;
-//             Database db = new Database("niisku.lamk.fi", "laurtomi", "Koodaus1", "user_laurtomi");
-//             TcpListener listener = new TcpListener(IPAddress.Any,portNum);
-//              
-//             listener.Start();
-// 
-//             Console.Write("Waiting for connection...");
-//             TcpClient client = listener.AcceptTcpClient();
-//             Console.WriteLine("Connection accepted.");
-// 
-//             while (!done)
-//             { 
-//                 NetworkStream ns = client.GetStream();
-//                 string message = Networking.ReadMessage(ns);
-//                 //db.AddRow(db.Parse(message));
-//                 //Console.WriteLine();
-//                 if (message == "end")
-//                 {
-//                     done = true;
-//                 }
-// 
-//                 Console.WriteLine(message);
-//             }
-//             Console.WriteLine("stopping");
-//             listener.Stop();
-
+            bool done = false;
             Database db = new Database("niisku.lamk.fi", "laurtomi", "Koodaus1", "user_laurtomi");
-            //List<PhoneRecord> records = db.GetFromColumnByValue("ds19_phonenumbers", "firstname", "Tomi");
-            List<PhoneRecord> records = db.GetAllEntries("ds19_phonenumbers");
-            foreach (var record in records)
-            {
-                Console.WriteLine(record.ToString());
+            TcpListener listener = new TcpListener(IPAddress.Any,portNum);
+             
+            listener.Start();
+
+            Console.Write("Waiting for connection...");
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Connection accepted.");
+
+            while (!done)
+            { 
+                NetworkStream ns = client.GetStream();
+                string message = Networking.ReadMessage(ns);
+                //db.AddRow(db.Parse(message));
+                //Console.WriteLine();
+
+                string[] messageArr = db.Parse(message);
+                switch (messageArr[0])
+                {
+                    case "end":
+                        done = true;
+                        break;
+                }
+
+                Console.WriteLine(message);
             }
+            Console.WriteLine("stopping");
+            listener.Stop();
+
+            //Database db = new Database("niisku.lamk.fi", "laurtomi", "Koodaus1", "user_laurtomi");
+            //List<PhoneRecord> records = db.GetFromColumnByValue("ds19_phonenumbers", "firstname", "Tomi");
+            //List<PhoneRecord> records = db.GetAllEntries("ds19_phonenumbers");
+            //foreach (var record in records)
+            //{
+            //    Console.WriteLine(record.ToString());
+            //}
 
 
             return 0;
