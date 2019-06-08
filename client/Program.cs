82e933ea64057afdd20974339a57b29a1ab169fd
Viisaspulla <30913249+Viisaspulla@users.noreply.github.com>
2019-06-08 13:11:38 +0300

    Added client commands.

--- client/Program.cs
+++ client/Program.cs
@@ -12,12 +12,67 @@ public class TcpTimeClient
         try
         {
             TcpClient client = new TcpClient(hostName, portNum);
-
             NetworkStream ns = client.GetStream();
 
             //Console.WriteLine(ReadMessage(ns));
+            //Networking.SendMessage(ns, "Hello Server!");
 
-            Networking.SendMessage(ns, "Hello Server!");
+            bool done = false;
+            while(!done)
+            {
+                string input = String.Empty;
+                int choice = -1;
+                Console.Write("To add new row select 0\n" +
+                              "To delete row select: 1\n" +
+                              "To get all rows select 2\n" +
+                              "To search row by name select 3\n" +
+                              "To search row by number select 4\n" +
+                              "To end the program select 5\n");
+                Console.Write("Enter your choice: ");
+                input = Console.ReadLine();
+                try
+                {
+                    choice = Int32.Parse(input);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"Unable to parse '{input}'");
+                    continue;
+                }
+
+                RequestType request = (RequestType)choice;
+                switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine("Case 0");
+                        Networking.SendMessage(ns, request.ToString());
+                        break;
+                    case 1:
+                        Console.WriteLine("Case 1");
+                        Networking.SendMessage(ns, request.ToString());
+                        break;
+                    case 2:
+                        Console.WriteLine("Case 2");
+                        Networking.SendMessage(ns, request.ToString());
+                        break;
+                    case 3:
+                        Console.WriteLine("Case 3");
+                        Networking.SendMessage(ns, request.ToString());
+                        break;
+                    case 4:
+                        Console.WriteLine("Case 4");
+                        Networking.SendMessage(ns, request.ToString());
+                        break;
+                    case 5:
+                        Console.WriteLine("Case 5");
+                        done = true;
+                        break;
+                    default:
+                        Console.WriteLine("Default case");
+                        break;
+                }
+            }
+
             client.Close();
         }
         catch (Exception e)
@@ -27,6 +82,4 @@ public class TcpTimeClient
 
         return 0;
     }
-
-   
 }
--- server/Program.cs
+++ server/Program.cs
@@ -11,40 +11,25 @@ namespace server
 
         public static int Main(String[] args)
         {
-            //         bool done = false;
-            // 
-            //         TcpListener listener = new TcpListener(IPAddress.Any,portNum);
-            // 
-            //         listener.Start();
-            // 
-            //         while (!done)
-            //         {
-            //             Console.Write("Waiting for connection...");
-            //             TcpClient client = listener.AcceptTcpClient();
-            // 
-            //             Console.WriteLine("Connection accepted.");
-            //             NetworkStream ns = client.GetStream();
-            // 
-            //             //             byte[] byteTime = Encoding.ASCII.GetBytes(DateTime.Now.ToString());
-            //             // 
-            //             //             try
-            //             //             {
-            //             //                 ns.Write(byteTime, 0, byteTime.Length);
-            //             //                 ns.Close();
-            //             //                 client.Close();
-            //             //             }
-            //             //             catch (Exception e)
-            //             //             {
-            //             //                 Console.WriteLine(e.ToString());
-            //             //             }
-            // 
-            //             Console.WriteLine(Networking.ReadMessage(ns));
-            //         }
-            // 
-            //         listener.Stop();
+            bool done = false;
+             
+            TcpListener listener = new TcpListener(IPAddress.Any,portNum);
+             
+            listener.Start();
+             
+            while (!done)
+            {
+                Console.Write("Waiting for connection...");
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("Connection accepted.");
+                NetworkStream ns = client.GetStream();
+                Console.WriteLine(Networking.ReadMessage(ns));
+            }
+             
+            listener.Stop();
 
-            Database db = new Database("niisku.lamk.fi", "laurtomi", "Koodaus1", "user_laurtomi");
-            db.GetAllEntries("ds19_phonenumbers ");
+            //Database db = new Database("niisku.lamk.fi", "laurtomi", "Koodaus1", "user_laurtomi");
+            //db.GetAllEntries("ds19_phonenumbers ");
 
 
             return 0;
