93c1be334bb4764ce559d57206b6b804f6e3dd6a
Viisaspulla <30913249+Viisaspulla@users.noreply.github.com>
2019-07-26 14:18:06 +0300

    Added settings file.

--- server/Program.cs
+++ server/Program.cs
@@ -13,91 +13,82 @@ namespace server
         public static int Main(String[] args)
         {
             bool done = false;
-            Database db = new Database("niisku.lamk.fi", "laurtomi", "Koodaus1", "user_laurtomi");
+            Database db = new Database(Settings.addr, Settings.user, Settings.pw, Settings.db);
             TcpListener listener = new TcpListener(IPAddress.Any, portNum);
 
-                listener.Start();
+            listener.Start();
 
-                Console.Write("Waiting for connection...");
-                TcpClient client = listener.AcceptTcpClient();
-                Console.WriteLine("Connection accepted.");
+            Console.Write("Waiting for connection...");
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Connection accepted.");
 
-                while (!done)
+            while (!done)
+            {
+                NetworkStream ns = client.GetStream();
+                string message = Networking.ReadMessage(ns);
+                //db.AddRow(db.Parse(message));
+                //Console.WriteLine();
+
+                string[] messageArr = db.Parse(message);
+                switch (messageArr[0])
                 {
-                    NetworkStream ns = client.GetStream();
-                    string message = Networking.ReadMessage(ns);
-                    //db.AddRow(db.Parse(message));
-                    //Console.WriteLine();
-
-                    string[] messageArr = db.Parse(message);
-                    switch (messageArr[0])
-                    {
-                        case "add_rows":
+                    case "add_rows":
+                        {
+                            int result = db.AddRow(message);
+                            Networking.SendMessage(ns, result + " rows added!");
+                            break;
+                        }
+                    case "delete_rows":
+                        {
+                            int result = db.DelRow(message);
+                            Networking.SendMessage(ns, result + " rows deleted!");
+                            break;
+                        }
+                    case "search_all_rows":
+                        {
+                            List<PhoneRecord> records = db.GetAllEntries("ds19_phonenumbers");
+                            Networking.SendMessage(ns, String.Join(",", records));
+                            break;
+                        }
+                    case "search_rows_by_name":
+                        {
+                            List<PhoneRecord> records = db.GetFromColumnByValue("ds19_phonenumbers", "lastname", messageArr[1]);
+                            if(records.Count == 0)
                             {
-                                int result = db.AddRow(message);
-                                Networking.SendMessage(ns, result + " rows added!");
-                                break;
+                                Networking.SendMessage(ns, String.Join(",", "null"));
                             }
-                        case "delete_rows":
+                            else
                             {
-                                int result = db.DelRow(message);
-                                Networking.SendMessage(ns, result + " rows deleted!");
-                                break;
-                            }
-                        case "search_all_rows":
-                            {
-                                List<PhoneRecord> records = db.GetAllEntries("ds19_phonenumbers");
                                 Networking.SendMessage(ns, String.Join(",", records));
-                                break;
                             }
-                        case "search_rows_by_name":
+                            break;
+                        }
+                    case "search_rows_by_number":
+                        {
+                            List<PhoneRecord> records = db.GetFromColumnByValue("ds19_phonenumbers", "phonenumber", messageArr[1]);
+                            if (records.Count == 0)
                             {
-                                List<PhoneRecord> records = db.GetFromColumnByValue("ds19_phonenumbers", "lastname", messageArr[1]);
-                                if(records.Count == 0)
-                                {
-                                    Networking.SendMessage(ns, String.Join(",", "null"));
-                                }
-                                else
-                                {
-                                    Networking.SendMessage(ns, String.Join(",", records));
-                                }
-                                break;
+                                Networking.SendMessage(ns, String.Join(",", "null"));
                             }
-                        case "search_rows_by_number":
+                            else
                             {
-                                List<PhoneRecord> records = db.GetFromColumnByValue("ds19_phonenumbers", "phonenumber", messageArr[1]);
-                                if (records.Count == 0)
-                                {
-                                    Networking.SendMessage(ns, String.Join(",", "null"));
-                                }
-                                else
-                                {
-                                    Networking.SendMessage(ns, String.Join(",", records));
-                                }
-                                break;
+                                Networking.SendMessage(ns, String.Join(",", records));
                             }
+                            break;
+                        }
 
-                        case "end":
-                            {
-                                done = true;
-                                break;
-                            }
-                    }
-
-                    Console.WriteLine(message);
+                    case "end":
+                        {
+                            done = true;
+                            break;
+                        }
                 }
-                Console.WriteLine("stopping");
-                listener.Stop();
 
-                //Database db = new Database("niisku.lamk.fi", "laurtomi", "Koodaus1", "user_laurtomi");
-                //List<PhoneRecord> records = db.GetFromColumnByValue("ds19_phonenumbers", "firstname", "Tomi");
-                //List<PhoneRecord> records = db.GetAllEntries("ds19_phonenumbers");
-                //foreach (var record in records)
-                //{
-                //    Console.WriteLine(record.ToString());
-                //}
-        
- 
+                Console.WriteLine(message);
+            }
+            Console.WriteLine("stopping");
+            listener.Stop();
+            
             return 0;
         }
 
---
+++ server/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace server
+{
+    static class Settings
+    {
+        public static string addr = "address";
+        public static string user = "user";
+        public static string pw = "password";
+        public static string db = "db";
+    }
+}
